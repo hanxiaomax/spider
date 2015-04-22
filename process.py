382a5a9bbcf84153134191f7691268bfc015a2c7
git config --global user.name hanxiaomaxgit config --global user.name <hanxiaomax@qq.com>
2015-04-22 22:05:22 +0800

    迁移到BS4

--- process.py
+++ process.py
@@ -1,22 +1,30 @@
 #coding:utf-8
 import re
 from datetime import datetime
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import os
 
 
 with open(u'teacher_pages/张志胜.html','r') as f:
     html=f.read()
     soup=BeautifulSoup(html)
+
     articlebody=soup.find("div",attrs={"class" : "Article_Content"})
 
-    reg = re.compile("<[^>]*>")
-    content = reg.sub('',articlebody.prettify())
-        #print content
+    tables=articlebody.find_all("table")
+    for table in tables:
+        print table.__class__
+        with open(u'teacher_pages/张志胜-body.txt','a') as fb:
+            content=unicode(table.get_text()).encode('utf-8')
+            fb.write(content)
 
-        # while t.nextSibling.string is not None:
-        #     print t.nextSibling.string
-    with open(u'teacher_pages/张志胜-body.txt','a') as fb:
+    # for t in tables:
+    #     print t.get_text()
+    #     #print content
 
-            # fb.write(articlebody.prettify())
-        fb.write(content)
+    #     # while t.nextSibling.string is not None:
+    #     #     print t.nextSibling.string
+    #     with open(u'teacher_pages/张志胜-body.html','w') as fb:
+
+    #         # fb.write(articlebody.prettify())
+    #         fb.write(content)
